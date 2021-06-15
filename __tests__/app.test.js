4d26f76d8c082990045972a9fa79b00bf02c76b2
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-14 19:53:47 -0700

    6 http requests working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,24 +4,15 @@ const app = require('../lib/app');
 describe('app routes', () => {
   it('it gets a string hi from /', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toEqual(
-      `HTTP/1.1 200 OK\r
-Content-Type: text/plain\r
-\r
-hi`);
+    expect(res.text).toEqual('hi');
   });
   it('it gets a body from /echo', async () => {
     const res = await request(app).get('/echo');
-    expect(res.text).toEqual(`HTTP/1.1 200 OK\r
-    Content-Type: text/plain\r
-    hi`);
+    expect(res.text).toEqual('hello');
   });
   it('it gets color red from /red', async () => {
     const res = await request(app).get('/red');
-    expect(res.text).toEqual(`HTTP/1.1 200 OK\r
-    Content-Type: text/plain\r
-    \r
-    hi`);
+    expect(res.text).toEqual('<h1>red<h1>');
   });
 
 

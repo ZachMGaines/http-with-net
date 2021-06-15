ffcdbc351128f07c53514d88f943e276a824473d
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-15 11:14:46 -0700

    all tests done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,8 @@ describe('app routes', () => {
     expect(res.text).toEqual('hi');
   });
   it('it gets a body from /echo', async () => {
-    const res = await request(app).get('/echo');
-    expect(res.text).toEqual('hello');
+    const res = await request(app).post('/echo').send('something');
+    expect(res.text).toEqual('something');
   });
   it('it gets color red from /red', async () => {
     const res = await request(app).get('/red');

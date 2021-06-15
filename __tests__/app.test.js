3411032db2bffa256f1a68cf5ab98fb06ed8ec8f
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-14 19:44:53 -0700

    WHY wont you workkkkkk

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,30 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  it('', () => {
-
+  it('it gets a string hi from /', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toEqual(
+      `HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+\r
+hi`);
   });
+  it('it gets a body from /echo', async () => {
+    const res = await request(app).get('/echo');
+    expect(res.text).toEqual(`HTTP/1.1 200 OK\r
+    Content-Type: text/plain\r
+    hi`);
+  });
+  it('it gets color red from /red', async () => {
+    const res = await request(app).get('/red');
+    expect(res.text).toEqual(`HTTP/1.1 200 OK\r
+    Content-Type: text/plain\r
+    \r
+    hi`);
+  });
+
+
+
+
+
 });
